b9362eb07e99a2ebccc5168b3c03348988817f3d
qkdrmawll <eunji8106@naver.com>
2024-05-29 16:09:07 +0900

    fix

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -44,6 +44,14 @@ git pull origin 브랜치명
 
 # git 작업 취소
 # working directory에서의 취소
+git checkout .
+
 # staging area에서의 취소
+git reset
+
 # commit 이후의 취소
+git reset HEAD~1
+git reset HEAD^
+
 # push 이후의 취소
+git revert 커밋ID
